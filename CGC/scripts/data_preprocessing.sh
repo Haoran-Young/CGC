628a32c31845219686cf2a4b5bc9e7e2d2e0f918
Haoran Yang <yanghaoran.1998@outlook.com>
2023-12-24 21:59:14 +0800

    Delete CGC directory

--- CGC/scripts/data_preprocessing.sh
+++
@@ -1,6 +0,0 @@
-#python ../codes/data_preprocessing.py
-#python ../codes/data_preprocessing.py --dataset AIDS
-#python ../codes/data_preprocessing.py --dataset FRANKENSTEIN
-#python ../codes/data_preprocessing.py --dataset Synthie
-#python ../codes/data_preprocessing.py --dataset ENZYMES
-python ../codes/data_preprocessing.py --dataset AIDS
