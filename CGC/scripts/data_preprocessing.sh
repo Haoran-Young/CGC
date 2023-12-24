7a2e13271cf5531c543582f80d4bb06044a75b20
Haoran Yang <yanghaoran.1998@outlook.com>
2023-12-24 21:58:15 +0800

    Add files via upload

---
+++ CGC/scripts/data_preprocessing.sh
@@ -0,0 +1,6 @@
+#python ../codes/data_preprocessing.py
+#python ../codes/data_preprocessing.py --dataset AIDS
+#python ../codes/data_preprocessing.py --dataset FRANKENSTEIN
+#python ../codes/data_preprocessing.py --dataset Synthie
+#python ../codes/data_preprocessing.py --dataset ENZYMES
+python ../codes/data_preprocessing.py --dataset AIDS
